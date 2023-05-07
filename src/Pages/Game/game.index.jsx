c33f3994d1ab3add01426e698693c0dcc963ec9c
Kathryn Jerez <kathrynjerez@gmail.com>
2023-05-07 16:36:26 -0400

    add loading to game page

--- src/Pages/Game/game.index.jsx
+++ src/Pages/Game/game.index.jsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import GameCard from "../../components/GameCard/GameCard";
 import Button from "react-bootstrap/Button";
 import { useParams } from "react-router-dom";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 
 const GamePage = () => {
@@ -14,6 +15,7 @@ const GamePage = () => {
   const priceData = queryParameters.get("price");
   const selectedPlayer = queryParameters.get("selectedPlayer");
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
 
 
   const searchUrl = async (locationData, priceData) => {
@@ -21,9 +23,11 @@ const GamePage = () => {
     try {
       const { data } = await axios.get(url);
       setFetchData(data.data);
+      setIsLoading(false);
     } catch (err) {
       console.log(err);
       toast.error("Something went wrong");
+      setIsLoading(false);
     }
   };
 
@@ -34,8 +38,19 @@ const GamePage = () => {
 
   return (
     <div className="game-container">
-
+      {isLoading ? (
+      <div className="waiting-for-players">
+        <p color="white">Loading...</p>
+        <Player
+          src="https://lottie.host/b985eb8b-3144-403d-9bfe-648398293810/We2wBFziNC.json"
+          className="player"
+          loop
+          autoplay
+        />
+      </div>
+      ) : (
       <GameCard cardData={fetchData} setCardData={setFetchData} selectedPlayer={selectedPlayer} gameId={id} />
+      )}
     </div>
   );
 };
